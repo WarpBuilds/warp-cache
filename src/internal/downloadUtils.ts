63ad45f77aeded9be5039179e5e577a01a1de6f9
Abhijit Hota <abhihota025@gmail.com>
2026-07-27 18:13:50 +0530

    chore: fix vulns (#20)
    * bump versions
    
    * fix

--- src/internal/downloadUtils.ts
+++ src/internal/downloadUtils.ts
@@ -1,7 +1,6 @@
 import * as core from '@actions/core'
 import {HttpClient, HttpClientResponse} from '@actions/http-client'
 import {BlockBlobClient} from '@azure/storage-blob'
-import {TransferProgressEvent} from '@azure/ms-rest-js'
 import * as buffer from 'buffer'
 import * as fs from 'fs'
 import * as stream from 'stream'
@@ -15,6 +14,8 @@ import {retryHttpClientResponse} from './requestUtils'
 import {AbortController} from '@azure/abort-controller'
 import {Storage, TransferManager} from '@google-cloud/storage'
 import {ChildProcessWithoutNullStreams, spawn} from 'child_process'
+
+type TransferProgressEvent = {loadedBytes: number}
 
 /**
  * Pipes the body of a HTTP response to a stream
